41045c0e48736e3e8aaa1cfb13e5cacfe6912cf3
SlayerK2024 <157855730+SlayerK2024@users.noreply.github.com>
2024-07-30 18:25:23 -0400

    Update install.js

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -11,19 +11,16 @@ window.addEventListener('beforeinstallprompt', (event) => {
 
 // TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
-  if (deferredPrompt) {
-        deferredPrompt.prompt();
-        const { outcome } = await deferredPrompt.userChoice;
-        if (outcome === 'accepted') {
-          console.log('User accepted the install prompt');
-        } else {
-          console.log('User dismissed the install prompt');
-        }
-        deferredPrompt = null;
-        butInstall.style.display = 'none';
-      }
-});
+ 
+  const promptEvent = window.deferredPrompt;
 
+  if (!promptEvent) {
+   return;
+  }
+  promptEvent.prompt();
+  window.deferredPrompt = null;
+  butInstall.classList.toggle('hidden', true);
+});
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
   window.deferredPrompt = null;
