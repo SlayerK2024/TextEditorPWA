f01ab7b20e6523a48081735b7d6eea6e1b160384
Kyna P <157855730+SlayerK2024@users.noreply.github.com>
2024-07-30 19:26:42 -0400

    update

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -66,6 +66,7 @@ module.exports = () => {
             loader: 'babel-loader',
             options: {
               presets: ['@babel/preset-env', '@babel/preset-react'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
             },
           },
         },
